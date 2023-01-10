503537925f6bc1883d87e28b970f314fd39b4fa5
AnnaGolitsyna <110576gav@gmail.com>
2023-01-10 19:08:35 +0200

    search

--- lesson3/task1/src/search.jsx
+++ lesson3/task1/src/search.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 
-const Search = (props) => {
+
+export default (props) => {
   return (
     <div className="search">
       <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
@@ -13,4 +13,4 @@ const Search = (props) => {
   );
 };
 
-export default Search;
+// export default Search;
