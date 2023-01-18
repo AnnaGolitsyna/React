18a3530bfcd4a93d419477c7b5a3f1198c65be11
AnnaGolitsyna <110576gav@gmail.com>
2023-01-18 07:31:34 +0200

    import

--- lesson5/task1/src/index.jsx
+++ lesson5/task1/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import GoodButton from './GoodButton.jsx';
+import './index.scss';
 
 const rootElem = document.querySelector('#root');
 
