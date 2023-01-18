fbefd5c4290f04d1b1c47296cd050de0464ac60e
AnnaGolitsyna <110576gav@gmail.com>
2023-01-18 07:50:36 +0200

    app

--- lesson5/task2/src/index.jsx
+++ lesson5/task2/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import GoodButton from './GoodButton';
+import App from './App';
 
 const rootElem = document.querySelector('#root');
 
-ReactDOM.render(<GoodButton />, rootElem);
+ReactDOM.render(<App />, rootElem);
