552f188f610de789331c547c93d84850517e708f
AnnaGolitsyna <110576gav@gmail.com>
2023-01-25 18:50:01 +0200

    task5

---
+++ lesson8/task5/src/ConnectionsStatus.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+class ConnectionsStatus extends React.Component {
+  state = {
+    status: 'Online',
+  };
+
+  componentDidMount() {
+    window.addEventListener('online', this.isOnline);
+    window.addEventListener('offline', this.isOffline);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.isOnline);
+    window.removeEventListener('offline', this.isOffline);
+  }
+
+  isOnline = () => {
+    this.setState({
+      status: 'Online',
+    });
+  };
+
+  isOffline = () => {
+    this.setState({
+      status: 'Offline',
+    });
+  };
+
+  render() {
+    const onlineStatus = this.state.status === 'Online';
+    return (
+      <>
+        <div className={onlineStatus ? 'status' : 'status status_offline'}>
+          {this.state.status}
+        </div>
+      </>
+    );
+  }
+}
+
+export default ConnectionsStatus;
---
+++ lesson8/task5/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import ConnectionsStatus from './ConnectionsStatus';
+
+const rootElem = document.querySelector('#root');
+
+ReactDOM.render(<ConnectionsStatus />, rootElem);
