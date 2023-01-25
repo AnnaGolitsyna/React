0dd7188f204413b73ffec100b7dfa271f59fa2ca
AnnaGolitsyna <110576gav@gmail.com>
2023-01-25 06:56:17 +0200

    task1

---
+++ lesson8/task1/src/Life.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class Life extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      num: 1,
+    };
+    console.log('constructor: good place to create state');
+  }
+  componentDidMount() {
+    console.log('componentDidMount: API calls, subscriptions');
+    this.setState({
+      num: Math.random(),
+    });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(nextProps, nextState);
+
+      console.log(
+        'shouldComponentUpdate(nextProps, nextState): decide to render or not to render'
+      );
+
+  }
+
+  componentDidUpdate(nextProps, prevState) {
+    console.log(nextProps, prevState);
+    console.log(
+      'componentDidUpdate(nextProps, prevState): some updates based on new props'
+    );
+    // this.setState({
+    //   num: Math.random() + 1,
+    // });
+  }
+
+  componentWillUnmount() {
+    console.log(
+      'componentWillUnmount(): cleanup befor DOM related to component will be removed'
+    );
+  }
+
+  render() {
+    console.log('return React element to build DOM');
+    return (
+      <>
+        <div>{this.state.num}</div>
+      </>
+    );
+  }
+}
+export default Life;
