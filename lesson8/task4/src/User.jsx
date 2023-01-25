1d78b042a1423bc8ad3d2392e5bcd7629afb9efb
AnnaGolitsyna <110576gav@gmail.com>
2023-01-25 14:33:49 +0200

    task4

---
+++ lesson8/task4/src/User.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class User extends React.Component {
+  state = {
+    user: null,
+  };
+
+  componentDidMount() {
+    fetch(`http://api.github.com/users/${this.props.userId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          user: data,
+        });
+      });
+  }
+
+  render() {
+    if (!this.state.user) {
+      return null;
+    }
+
+    const { name, location, avatar_url } = this.state.user;
+    return (
+      <div className="user">
+        <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+        <div className="user__info">
+          <span className="user__name">{name}</span>
+
+          <span className="user__location">{location}</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default User;
