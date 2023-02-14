935416b7f891e94bb2a800a3b31d0a3eea844394
AnnaGolitsyna <110576gav@gmail.com>
2023-02-14 12:24:15 +0200

    rename

--- lesson14/task3/src/index.jsx
+++ lesson14/task3/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Dimensions from './Dimensios';
+import Dimensions from './Dimensions';
 
 const rootElem = document.querySelector('#root');
 
